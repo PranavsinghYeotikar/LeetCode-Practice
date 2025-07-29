5477c8769bd72e65e5e64b02dbde55b8194520b0
Pranavsingh Yeotikar <pranavsinghyeotikar2005@gmail.com>
2025-07-29 12:50:10 +0530

    MaxProductSubarray done

--- Blind75/MaxProductSubarray.java
+++ Blind75/MaxProductSubarray.java
@@ -1,12 +1,31 @@
-public class MaxProductSubarray {
+public class MaxProductSubarray{
     public static void main(String[] args) {
-        int[] nums = {2,3,-2,4};
-        int mul = nums[0];
-        int maxMul = nums[0];
+        int[] nums = {-2,0,-1};
+        int max = nums[0];
+        int pro = 1;
+
         for(int i=0;i<nums.length;i++){
-            if()
+            if(nums[i] == 0){
+                pro = 1;
+                max = Math.max(max, 0);
+            }
+            else{
+                pro = pro * nums[i];
+                max = Math.max(pro, max);
+            }
         }
-        
-        System.out.println(maxMul);
+        pro = 1;
+        for(int i=nums.length-1;i>=0;i--){
+            if(nums[i] == 0){
+                pro = 1;
+                max = Math.max(0, max);
+            }
+            else{
+                pro = pro * nums[i];
+                max = max = Math.max(pro, max);
+            }
+        }
+
+        System.out.println(max);
     }
 }
