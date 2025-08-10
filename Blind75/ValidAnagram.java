e45b952e0b752a83518925919153eb4338f0784d
Pranavsingh Yeotikar <pranavsinghyeotikar2005@gmail.com>
2025-08-10 12:52:13 +0530

    valid anagram doone

--- Blind75/ValidAnagram.java
+++ Blind75/ValidAnagram.java
@@ -1,15 +1,29 @@
-public class ValidAnagram{
-    public static boolean findAnagram(String s, String t){
-        if(s.length() != t.length()) return false;
+import java.util.Arrays;
 
-        for(int i=0;i<s.length();i++){
+public class ValidAnagram {
+    public static boolean findAnagram(String s, String t) {
+        if (s.length() != t.length())
+            return false;
+
+        char[] chars = s.toCharArray();
+        Arrays.sort(chars);
+        String sorted = new String(chars);
+
+        char[] chars2 = t.toCharArray();
+        Arrays.sort(chars2);
+        String sorted2 = new String(chars2);
+
+        if (sorted.equals(sorted2)) {
+            return true;
+        } else {
+            return false;
         }
-
-        return false;
     }
+
     public static void main(String[] args) {
         String s = "anagram";
-        String t = "nagaram";
+        String t = "nagaraj";
         boolean ans = findAnagram(s, t);
+        System.out.println(ans);
     }
 }
